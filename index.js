8ead07c9d99a01ad583057913174290d39aa49b5
Zenen Avila <zenenavilasuarez@gmail.com>
2021-09-23 00:19:11 -0500

    api funcionando correctamente sin recibir parametros

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-////////////  configuracion API ////////////
-
+////////////  Requerimientos ////////////
 const { Pool } = require('pg');
+
+const morgan = require('morgan');
 
 const express=require('express');
 const app=express();
-const bodyParser = require('body-parser');
+
+//const bodyParser = require('body-parser');
+
+////////////  configuracion API ////////////
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 const port=process.env.PORT||5000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended:true
-}));
-
 
 ////////////  configuracion database ////////////
 
@@ -25,61 +26,6 @@ const config={
 
 };
 const pool = new Pool(config);
-////////////  configuracion consultas ////////////
-
-
-const getUsuarios = async () => {
-    try {
-        const res = await pool.query({rowMode:'array',
-        text:'select * from usuarios;'});
-        //console.log(res.rows);
-        //pool.end();
-        var usuarios=`${res.fields[0].name}: ${res.fields[0]}
-        ${res.fields[1].name}: ${res.fields[1]}` ;
-        return usuarios;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const insertUsuarios = async () => {
-    try {
-        var contraseña = utf8_encode("contraseña")
-        const text=`insert into usuarios(nombre,apellidos,password)
-        values('oliver','avila trani','${contraseña}')`;
-
-        const res = await pool.query(text)
-        console.log(res);
-        //return res;
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const deleteUsuarios = async () => {
-    try {
-        const text = `delete from usuarios where nombre ='zenen'` ;   
-        const res = await pool.query(text);
-        console.log(res);
-        //return res;
-
-    } catch (error) {
-        console.log(error);
-    }
-}
-const updateUser = async () => {
-    try {
-        const text=`update usuarios set password = 'prueba2' where
-        nombre='adolfo'`;
-        const res= await pool.query(text);
-        console.log(res);
-        return res;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
 
 ////////////  configuracion endpoint ////////////
 
@@ -88,29 +34,73 @@ app.get('/',(request,response)=>{
 });
 
 //consultar usuarios
-app.get('/mostrar',(request,response)=>{
-    const res=getUsuarios();
-    
-    console.log(res);
-    response.json({info:res});
+app.get('/mostrartodos',(request,response)=>{
+
+    pool.query(`select * from usuarios 
+    order by id asc; `,(err,res)=>
+    {
+        if(err){
+            response.json(err.stack);
+        }else{
+            response.json(res.rows);
+        }
+    });
 });
 
-//insertar usuarios
+var mostrar=`select id,nombre,apellidos,password
+from usuarios where eliminado=false order by id asc;`;
+
+app.get('/mostrar',(request,response)=>{
+
+        pool.query(mostrar,(err,res)=>
+        {
+            if(err){
+                response.json(err.stack);
+            }else{
+                response.json(res.rows);
+            }
+        });
+});
+
+//insertar usuariosn
 app.get('/insertar',(request,response)=>{
-    const res=insertUsuarios();
-    response.json({info:res});
+    var contraseña = btoa("contraseña");
+    pool.query(`insert into usuarios(nombre,apellidos,password)
+                values('oliver','avila trani','${contraseña}');`
+                ,(err,res)=>
+        {
+            if(err){
+                response.json(err.stack);
+            }else{
+                response.json("Insertado Correctamente");
+            }
+        });
 });
 
 //eliminar usuarios
 app.get('/eliminar',(request,response)=>{
-    const res=deleteUsuarios();
-    response.json({info:res});
+    pool.query(`update usuarios set eliminado=true 
+    where nombre ='oliver'`,(err,res)=>
+    {
+        if(err){
+            response.json(err.stack);
+        }else{
+            response.json("Eliminado Correctamente");
+        }
+    });
 });
 
 //actualizar usuarios
 app.get('/actualizar',(request,response)=>{
-    const res=updateUser();
-    response.json({info:res});
+    pool.query(`update usuarios set password = 
+    'prueba2' where nombre='bayron'`,(err,res)=>
+    {
+        if(err){
+            response.json(err.stack);
+        }else{
+            response.json("actualizar Correctamente");
+        }
+    });
 });
 
 app.listen(port,()=>{
