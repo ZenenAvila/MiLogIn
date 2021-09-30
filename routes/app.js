0f8414509230f58f7f690e12858297617b52ef8f
ZenenAvila <90022515+ZenenAvila@users.noreply.github.com>
2021-09-30 17:48:20 -0500

    Agregando headers en app

--- routes/app.js
+++ routes/app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 
-app.set('port',process.env.PORT||5000);
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(
@@ -12,16 +12,19 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,POST'); // If needed to add
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Origin,Accept,Authorization,x-access-token'); // If needed	
+	res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+	next();
   });
 
   app.get('/',(request,response)=>{
     response.json({info:'la API esta en ejecución'});
 });
 
-app.listen(app.get('port'),()=>{
-    console.log( `API corriendo en el puerto ${app.get('port')}`);
+app.listen(port,()=>{
+	console.log(`API corriendo en el puerto ${port} (env:${process.env.NODE_ENV})`);
 });
 
 
