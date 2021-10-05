98a609424b5d652b96f6ef7dd177ea5adf52892d
Zenen Avila <zenenavilasuarez@gmail.com>
2021-10-04 20:54:28 -0500

    cambiando modulo cors y reinstalando express

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const app=require('./routes/app');
 const usuariosRoutes=require('./routes/usuariosRoutes');
 
-/////////////////cors
-const cors=require('cors');
-app.use(cors());
 
 app.use('/usuarios',usuariosRoutes);
