24d19d9c11ff418d1c602db43a7f45606fa3184e
Zenen Avila <zenenavilasuarez@gmail.com>
2021-10-04 20:49:50 -0500

    agregando modulo corse

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const app=require('./routes/app');
 const usuariosRoutes=require('./routes/usuariosRoutes');
 
+/////////////////cors
+const cors=require('cors');
+app.use(cors());
 
 app.use('/usuarios',usuariosRoutes);
